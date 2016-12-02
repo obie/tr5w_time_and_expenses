565075bffc1afc089f3db30bc199c84842b06578
Obie Fernandez <obie@andela.co>
2016-12-02 13:40:30 -0500

    added optional true to billing codes and validation of scope

--- app/models/billable_week.rb
+++ app/models/billable_week.rb
@@ -1,9 +1,12 @@
 class BillableWeek < ApplicationRecord
+  attribute :start_date, :datetime, default: -> { Time.now }
+
   belongs_to :client
-  belongs_to :billing_code
+  belongs_to :billing_code, optional: true
   belongs_to :timesheet
 
   validates_presence_of :start_date
+  validates_uniqueness_of :client_id, scope: :timesheet_id
 
   def total_hours
     %w{ mon tues wednes thurs fri satur sun }.inject(0) do |acc,day|
